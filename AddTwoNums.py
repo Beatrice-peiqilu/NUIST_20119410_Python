3ef50ec1ced8d3d16f47a456472bd32100a44c57
陆佩琪 <3159897866@qq.com>
2026-03-23 18:55:06 +0800

    Update to Version 2 with user input

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,7 +1,7 @@
-# Add Two Numbers in Python
+# Version 2: Add Two Numbers with input
 # Author: lupeiqi
 
-num1 = 5
-num2 = 10
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 sum = num1 + num2
-print("Sum of", num1, "and", num2, "is", sum)
+print("Sum is", sum)
